33e9fe665c0ca8dfe50dcc74c9fa99e3364842ae
„Michal <s22288@pjwstk.edu.pl>
2022-12-12 18:09:18 +0100

    start adding css to project

--- tin-projekt-wcislo-s22288/controllers/movieController.js
+++ tin-projekt-wcislo-s22288/controllers/movieController.js
@@ -1,3 +1,9 @@
 exports.showMovieList = (req, res, next) => {
     res.render('movie/listOfMovies', {});
 };
+exports.showMovieListEmpty = (req, res, next) => {
+    res.render('movie/listOfMoviesEmpty', {});
+}
+exports.AddMovie = (req, res, next) => {
+    res.render('movie/movieForm', {});
+}
--- tin-projekt-wcislo-s22288/routes/movieRoute.js
+++ tin-projekt-wcislo-s22288/routes/movieRoute.js
@@ -5,5 +5,6 @@ const movieController = require('../controllers/movieController');
 
 // GET /movies
 router.get('/', movieController.showMovieList);
-
+router.get('/empty', movieController.showMovieListEmpty);
+router.get('/add', movieController.AddMovie);
 module.exports = router;
